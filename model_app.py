17a4609c740b7ccd39c8d5f566140881bc45c444
Kelvin Rotich <141912522+Kelvin-Rotich824@users.noreply.github.com>
2023-11-29 16:46:38 +0300

    Finished notebook

--- model_app.py
+++ model_app.py
@@ -3,22 +3,23 @@ import streamlit as st
 import pandas as pd
 import joblib
 from xgboost import XGBClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 # Load the trained model
-your_model = joblib.load('customer_churn_model.pkl') 
+model = joblib.load('customer_churn_model.pkl') 
 
 # Streamlit app code
 st.title("Your Model Deployment App")
 
 # Add input components (example: file upload)
-uploaded_file = st.file_uploader("customer_churn", type=["csv"])
+uploaded_file = st.file_uploader("file")
 if uploaded_file is not None:
-    input_data = pd.read_csv(uploaded_file).drop(['state', 'area code', 'phone number'], axis=1)
+    input_data = pd.read_csv(uploaded_file)
     st.write("Input Data:")
     st.dataframe(input_data)
 
     # Make predictions using the trained model
-    predictions = your_model.predict(input_data)
+    predictions = model.predict(input_data)
 
     # Display the predictions
     st.write("Predictions:")
