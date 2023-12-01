f1913d361f5b82070fb1550fb141b86baaaf37f4
Kelvin Rotich <141912522+Kelvin-Rotich824@users.noreply.github.com>
2023-12-01 10:19:39 +0300

    Updated python file

--- model_app.py
+++ model_app.py
@@ -6,32 +6,49 @@ import numpy as np
 import joblib
 
 def predict(features):
-    # Scale the features using the MinMaxScaler
-    minmax_scaler = MinMaxScaler()
-    scaled_features = minmax_scaler.fit_transform(features)
+    # Create a mapping function and apply it to the selected columns
+    def binary_feature(target_value):
+        if target_value == 'yes':
+            return 1
+        else:
+            return 0
 
-    # Make predictions using the XGBoost model
-    model = joblib.load('customer_churn_model.pkl')
-    prediction = model.predict(scaled_features)
+# Applying the function to the selected columns.
+    for column in data[['international plan', 'voice mail plan']]:
+        data[column] = data[column].apply(binary_feature)
+        print(f'\n{data[column].value_counts()}')
+        # Scaling the features using the MinMaxScaler
+        minmax_scaler = MinMaxScaler()
+        scaled_features = minmax_scaler.fit_transform(features)
 
-    return prediction
+        # Making predictions using the model
+        model = joblib.load('customer_churn_model.pkl')
+        y = [True, False]
+        for item in y:
+            model.fit(scaled_features, item)
+        prediction = model.predict(scaled_features)
+
+        return prediction
 
 def main():
-    st.title("XGBoost Model Deployment with Streamlit")
+    st.title("Model Deployment with Streamlit")
 
-    # Create input sliders for 16 features
-    feature_sliders = [st.slider(f"Feature {i+1}", 0.0, 1.0, 0.5) for i in range(17)]
+    # Creating input sliders for 17 features
+    feature_sliders = [st.slider(f"Feature {i+1}", 0.0, 1.0, 0.5) for i in range(18)]
 
-    # Create a button to make predictions
+    # Creating a button to make predictions
     if st.button("Make Prediction"):
-        # Prepare input features as a numpy array
+        # Preparing input features as a numpy array
         input_features = np.array(feature_sliders).reshape(1, -1)
 
-        # Get the prediction
+        # Getting the prediction
         model = joblib.load('customer_churn_model.pkl')
+        y = [True, False]
+        for item in y:
+            model.fit(input_features, item)
         prediction = model.predict(input_features)
 
-        # Display the prediction
+        # Displaying the prediction
         st.success(f"The prediction is: {prediction[0]}")
 
 if __name__ == "__main__":
